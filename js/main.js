d047fb6ec6c7a7d19e6e47e37dd7c3c1c62a5374
dan.padgett1@gmail.com <dan.padgett1@gmail.com>
2019-08-15 17:27:44 -0400

    adding header elements to sections

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-    //TODO
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top + 80
+        }, 500);
+    });
 });
